ab65612a2d663e77129f26cfe5b7463e40adbe4b
Phil <phetzel89@gmail.com>
2021-05-02 08:55:11 -0700

    Connect app with heroku

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,8 @@
 import { create } from 'apisauce';
 
 const apiClient = create({
-    baseURL: 'http://localhost:3000/api'
+    // baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://walk-rails-apiu.herokuapp.com/api'
 });
 
 export default apiClient;
