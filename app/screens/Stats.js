e86607c8f7db652c4fd872df530266ff810f5e38
Phil <phetzel89@gmail.com>
2021-08-17 19:35:39 -0700

    Stat Update Fix

--- app/screens/Stats.js
+++ app/screens/Stats.js
@@ -1,59 +1,58 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useContext, useEffect, useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
 
-import colors from '../util/colors';
-import Screen from '../components/Screen';
-import StatsDetails from '../components/StatsDetails';
-import SwitchActivity from '../components/SwitchActivity';
-import SwitchTimes from '../components/SwitchTimes';
-import UserContext from '../context/user_context';
-import { fetchWalks } from '../api/walk_api';
+import colors from "../util/colors";
+import Screen from "../components/Screen";
+import StatsDetails from "../components/StatsDetails";
+import SwitchActivity from "../components/SwitchActivity";
+import SwitchTimes from "../components/SwitchTimes";
+import UserContext from "../context/user_context";
+import WalkContext from "../context/walk_context";
+import { fetchWalks } from "../api/walk_api";
 
 const Stats = (props) => {
-    const { userId } = useContext(UserContext);
-    const [walks, setWalks] = useState();
-    const [time, setTime] = useState('all');
-    const [activity, setActivity] = useState();
+  const { userId } = useContext(UserContext);
+  const { onWalk } = useContext(WalkContext);
+  const [walks, setWalks] = useState();
+  const [time, setTime] = useState("all");
+  const [activity, setActivity] = useState();
 
-    useEffect(() => {
-        const obj = { user_id: userId, date: time,  activity: activity};
-        fetchWalks(obj).then(res => {
-            setWalks(res.data);
-        })
-    }, [time, activity])
+  useEffect(() => {
+    const obj = { user_id: userId, date: time, activity: activity };
+    fetchWalks(obj).then((res) => {
+      setWalks(res.data);
+    });
+  }, [time, activity, onWalk]);
 
-    return (
-        <Screen>
-            <View style={styles.container}>
-                <Text style={styles.header}>STATS</Text>
-                <SwitchTimes func={setTime} />
-                <SwitchActivity func={setActivity} />
- 
-                { walks &&
-                    <StatsDetails 
-                        activity={activity}
-                        time={time} 
-                        walks={walks} />
-                } 
-            </View>
-        </Screen>
-    );
+  return (
+    <Screen>
+      <View style={styles.container}>
+        <Text style={styles.header}>STATS</Text>
+        <SwitchTimes func={setTime} />
+        <SwitchActivity func={setActivity} />
+
+        {walks && (
+          <StatsDetails activity={activity} time={time} walks={walks} />
+        )}
+      </View>
+    </Screen>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
-      backgroundColor: colors.black,
-      flex: 1,
-      width: '100%'
+    backgroundColor: colors.black,
+    flex: 1,
+    width: "100%",
   },
-    header: {
-      alignSelf: 'center',
-      color: colors.white,
-      fontSize: 33,
-      fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-      marginBottom: 30,
-      marginTop: 30,
-  }
+  header: {
+    alignSelf: "center",
+    color: colors.white,
+    fontSize: 33,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    marginBottom: 30,
+    marginTop: 30,
+  },
 });
 
 export default Stats;
